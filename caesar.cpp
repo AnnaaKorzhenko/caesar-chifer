8929b64bce65a0fb5161d25a03d591f744dbf91c
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-06-18 17:05:49 +0300

    updated functions

--- caesar.cpp
+++ caesar.cpp
@@ -1,34 +1,34 @@
-#include "caesar.h"
-
-int search_index(char c) {
-    char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int i = 0;
-    while(alphabet[i] != c && i < 26) {
-        i++;
-    }
-    return i;
-}
-
-void encrypt(char* text, int key) {
-    char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int i;
-    for (i = 0; text[i] != '\0'; i++) {
-        int index = search_index(text[i]);
-        if (index < 26) {
-            text[index] = alphabet[(index + key) % 26];
+extern "C" {
+    __declspec(dllexport) int search_index(char c) {
+        char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int i = 0;
+        while(alphabet[i] != c && i < 26) {
+            i++;
         }
+        return i;
     }
-    text[i] = '\0';
-}
 
-void decrypt(char* text, int key) {
-    const char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int i;
-    for (i = 0; text[i] != '\0'; i++) {
-        int index = search_index(text[i]);
-        if (index < 26) {
-            text[i] = alphabet[(index - key + 26) % 26];
+    __declspec(dllexport) void encrypt(char* text, int key) {
+        char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int i;
+        for (i = 0; text[i] != '\0'; i++) {
+            int index = search_index(text[i]);
+            if (index < 26) {
+                text[index] = alphabet[(index + key) % 26];
+            }
         }
+        text[i] = '\0';
     }
-    text[i] = '\0';
+
+    __declspec(dllexport) void decrypt(char* text, int key) {
+        const char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int i;
+        for (i = 0; text[i] != '\0'; i++) {
+            int index = search_index(text[i]);
+            if (index < 26) {
+                text[i] = alphabet[(index - key + 26) % 26];
+            }
+        }
+        text[i] = '\0';
+    }
 }
